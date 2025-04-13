25daedca2edb9fe894351b8e7cd090b4b2b46350
rashid <rashid.best.ry@gmail.com>
2025-04-13 16:00:06 +0300

    little fixes

--- war_game.py
+++ war_game.py
@@ -1,4 +1,5 @@
 # CARD CLASS -----------------------------------------------------------------------------------------------------------
+
 suits = ("Hearts", "Diamonds", "Spades", "Clubs")
 values = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10,
           "Jack": 11,
@@ -12,7 +13,6 @@ class Card:
 
     def __str__(self):
         return self.rank + " of " + self.suit
-
 
 # DECK CLASS -----------------------------------------------------------------------------------------------------------
 
@@ -32,3 +32,17 @@ class Deck:
         return self.all_cards.pop()
 
 # PLAYER CLASS ---------------------------------------------------------------------------------------------------------
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.all_cards = []
+    def remove_one(self):
+        return self.all_cards.pop(0)
+    def add_cards(self,new_cards):
+        if type(new_cards)==type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+    def __str__(self):
+        return f"Player {self.name} has {len(self.all_cards)} cards"
