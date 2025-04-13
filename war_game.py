06344f59548b670caf1130391dd51906ee49263e
rashid <rashid.best.ry@gmail.com>
2025-04-13 15:14:30 +0300

    started

--- war_game.py
+++ war_game.py
@@ -1,18 +1,34 @@
-# CARD CLASS
+# CARD CLASS -----------------------------------------------------------------------------------------------------------
 suits = ("Hearts", "Diamonds", "Spades", "Clubs")
-dic = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 11,
-       "Queen": 12, "King": 13, "Ace": 14}
-
+values = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10,
+          "Jack": 11,
+          "Queen": 12, "King": 13, "Ace": 14}
 
 class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
-        self.values = dic[rank]
+        self.values = values[rank]
+
     def __str__(self):
-        return self.suit + "of" self.rank
+        return self.rank + " of " + self.suit
 
-card_temp = Card("Diamonsd","Seven")
 
-card_temp.suit
+# DECK CLASS -----------------------------------------------------------------------------------------------------------
 
+import random
+
+class Deck:
+    def __init__(self):
+        self.all_cards = []
+        for x in suits:
+            for y in values.keys():
+                self.all_cards.append(Card(x, y))
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.all_cards.pop()
+
+# PLAYER CLASS ---------------------------------------------------------------------------------------------------------
