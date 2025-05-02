b62408657369a7286d26f48c4c4fcba5c458c346
rashid <rashid.best.ry@gmail.com>
2025-05-02 20:13:49 +0300

    replacing entire code

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,78 +1,68 @@
-def welcome(key):
-    if key=="cont":
-        print("You wanna play again!")
+import random
+p = ["BOARD", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+players = {}
+def board_print():
+    print("******",p[0],"******")
+    board=[[p[1], p[2],p[3]],[p[4], p[5],p[6]],[p[7], p[8],p[9]]]
+    for x in board:
+        print(f"-------------------")
+        offs=[2,5,7]
+        for num,y in enumerate(x,1):print(end=f"|  {y}  | " if num in offs else f"  {y}   ")
+        print()
+    print("-------------------")
+def players_fun():
+    player_1 = int(input("Which one you prefer 1>X or 2>O : "))
+    if player_1 == 1:
+        players["player_1"] = "X"
+        players["player_2"] = "O"
     else:
-        print("\nWelcome to Tic Tac Toe: ")
-def confirm_play():
-    while True:
-        confirm = input("Are you ready to play? Enter Yes or NO: ")
-        if "N" == confirm:
-            return False
-        if "Y" == confirm:
+        players["player_1"] = "O"
+        players["player_2"] = "X"
+    print("\nLet's Play the game!")
+
+def checking():
+    cheks=[(1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7)]
+    for x,y,z in cheks:
+        if p[x]==p[y]==p[z] and p[x]!=" ":
             return True
-        else:
-            print("Please enter correct input!\n")
-    while True:
-        player1 = input("Do you want to be X or O : ")
-        if "X" == player1.upper():
-            player2 = "O"
-            break
-        if "O" == player1.upper():
-            player2 = "X"
-            break
-        else:
-            print("Please enter correct input!\n")
-    print("\nPlayer 1 will go first.")
-def start_game(key):
-    if key=="cont":
-        welcome("cont")
-    else:
-        welcome("new")
-    replay = confirm_play()
-    control = 1
-    status = False
-    liste = [[""], [""], [""], [""], [""], [""], [""], [""], [""]]
-    while replay == True:
-        jump = None
-        def choose_position():
-            if control % 2 == 0:
-                OX = "X"
-                print("Player1 Go on!!!")
-            if control % 2 == 1:
-                OX = "O"
-                print("Player2 Go on!!!")
-            choose = int(input("Choose your next position: (1-9)"))
-            if liste[choose] == "X":
-                print("Please mark empty box!!")
-                choose_position()
-            else:
-                liste[choose - 1] = OX
-                display_board()
-        def display_board():
-            ls = [[f"{liste[0][0]}", "|", f"{liste[1][0]}", "|", f"{liste[2][0]}"],
-                  [f"{liste[3][0]}", "|", f"{liste[4][0]}", "|", f"{liste[5][0]}"],
-                  [f"{liste[6][0]}", "|", f"{liste[7][0]}", "|", f"{liste[8][0]}"]]
-            for x in range(len(ls)):
-                print("----------------------")
-                for y in range(len(ls[0])):
-                    print(f"{ls[x][y]:^4}", end="")
-                print()
-            print("----------------------")
-        if liste[0] == liste[1] == liste[2] == "X":status=True
-        if liste[3] == liste[4] == liste[5] == "X":status=True
-        if liste[6] == liste[7] == liste[8] == "X":status=True
-        if liste[0] == liste[1] == liste[2] == "O":status=True
-        if liste[3] == liste[4] == liste[5] == "O":status=True
-        if liste[6] == liste[7] == liste[8] == "O":status=True
-        if status:
-            if control % 2 == 0:
-                winer = "Player1"
-            else:
-                winer = "Player2"
-            print(f"{winer} winner!!!!!")
-            replay=False
+    return False
+def game_engine():
+    r = random.choice(["player_1","player_2"])
+    counter=9
+    while counter>0:
+        print(f"Player {r[-1]} your turn {players[r]}")
+        board_print()
+        stamp=int(input("Mark from 1 to 9: "))
+        if p[stamp]=="X" or p[stamp]=="O":
+            print("\nPick another socket!")
             continue
-        choose_position()
-        control += 1
-    start_game("cont")
-start_game("new")
+        p[stamp]=players[r]
+        board_print()
+        counter-=1
+        if r=="player_1":r="player_2"
+        else:r="player_1"
+        if checking():break
+        else:continue
+    if checking():
+        print(f"\nCongratulations Player {r[-1]} win the game!!!")
+    else:
+        print(f"\nNo winners!")
+        start_game(state=True,cont=True)
+def start_game(state,cont=False):
+    global p
+    if state and not cont:
+        print("Welcome to Tic_Tac_Toe!!!")
+        p = ["BOARD", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    if state and cont:
+        print("\nLets Play again!!!")
+        p = ["BOARD", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    if not state:return "\nGame is Over!!!"
+    while state:
+        board_print()
+        players_fun()
+        game_engine()
+        condition=input("\nDid you wanna play again? Y or N : ")
+        print("Wrong! You gonna play!!!")
+        start_game(state=True,cont=True)
+if __name__=="__main__":
+    start_game(state=True,cont=None)
